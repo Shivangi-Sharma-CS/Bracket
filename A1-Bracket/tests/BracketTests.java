e4bba330c2c615fe5f77e5681ea675d83b9e16f2
Shivangi Sharma <ssharma3@upei.ca>
2020-02-12 18:46:53 -0400

    Uploaded Bracket AS1

---
+++ A1-Bracket/tests/BracketTests.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * A couple of sample unit tests that your code should pass
+ */
+public class BracketTests {
+    @Test
+    public void twoBracketRound1() {
+
+        Bracket b = new Bracket(2);
+
+        b.loadResults(1, "test-1.txt");
+        String s = b.toJSON();
+        System.out.println(s);
+        s = s.replaceAll("\\s","");
+        s = s.replaceAll("\\r","");
+        s = s.replaceAll("\\n","");
+        assertEquals(s, "{\"name\":\"-\",\"children\":[{\"name\":\"A\"},{\"name\":\"B\"}]}");
+    }
+
+    @Test
+    public void twoBracketRound2() {
+        Bracket b = new Bracket(2);
+
+        b.loadResults(1, "test-1.txt");
+        b.loadResults(2, "test-2.txt");
+        String s = b.toJSON();
+        System.out.println(s);
+        s = s.replaceAll("\\s","");
+        s = s.replaceAll("\\r","");
+        s = s.replaceAll("\\n","");
+        assertEquals(s, "{\"name\":\"A\",\"children\":[{\"name\":\"A\"},{\"name\":\"B\"}]}");
+    }
+}
